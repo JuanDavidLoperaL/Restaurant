9c0c564b3d1919d2f0f64fc2e91e6a76926fed3a
Juan David Lopera Lopez <juan.lopez@valid.com>
2022-01-13 21:06:21 -0500

    Develop branch created

---
+++ JuanRestaurant/JuanRestaurant/Welcome/View/WelcomeBaseView.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeBaseView.swift
+//  JuanRestaurant
+//
+//  Created by Juan David Lopera Lopez on 13/01/22.
+//
+
+import UIKit
+
+final class WelcomeBaseView: UIView {
+    // MARK: - Private UI Properties
+    private let restaurantNameLabel: UILabel = {
+        let label: UILabel = UILabel()
+        return label
+    }()
+    
+    private let customerAttentionLabel: UILabel = {
+        let label: UILabel = UILabel()
+        return label
+    }()
+}
+
